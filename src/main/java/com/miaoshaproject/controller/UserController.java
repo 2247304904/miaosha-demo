0ef7b2b9121c509f974a9a0a3f015505a873f830
张鹏宇 <2247304904@qq.com>
2022-04-04 22:22:24 +0800

    2022-4-4
    短信OPT

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 
 /**
@@ -30,6 +31,30 @@ public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private HttpServletRequest request;
+
+
+    //用户获取otp短信接口
+    @RequestMapping(value = "/getotp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telphone") String telphone) {
+
+        //需要按照一定的规则生成OTP验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //将OTP验证码同对应用户的手机号关联，使用httpsession的方式绑定手机号与OTPCDOE
+        request.getSession().setAttribute(telphone, otpCode);
+
+        //将OTP验证码通过短信通道发送给用户，省略
+        System.out.println("telphone=" + telphone + "&otpCode=" + otpCode);
+
+        return CommonReturnType.create(null);
+    }
+
 
     @RequestMapping("/get")
     @ResponseBody

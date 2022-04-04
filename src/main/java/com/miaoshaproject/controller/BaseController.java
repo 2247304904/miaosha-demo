ba0532b036147811b8b8f52f40a195285f4f3c61
张鹏宇 <2247304904@qq.com>
2022-04-04 20:39:22 +0800

    2022-4-4
    异常处理

---
+++ src/main/java/com/miaoshaproject/controller/BaseController.java
@@ -0,0 +1,42 @@
+package com.miaoshaproject.controller;
+
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBusinessError;
+import com.miaoshaproject.response.CommonReturnType;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @program: miaosha
+ * @description: 处理异常
+ * @author: 张鹏宇
+ * @create: 2022-04-04 20:37
+ **/
+
+public class BaseController {
+    //定义ExceptionHandler解决未被controller层吸收的Exception
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.OK) //返回前端200
+    @ResponseBody
+    public Object handleException(HttpServletRequest request, Exception ex) {
+        Map<String, Object> responseData = new HashMap<>();
+        if (ex instanceof BusinessException) {  //如果是自定义的已知异常
+            BusinessException businessException = (BusinessException) ex;
+            responseData.put("errCode", businessException.getErrorCode());
+            responseData.put("errMsg", businessException.getErrMsg());
+
+        } else {//如果是未定义的系统异常
+            responseData.put("errCode", EmBusinessError.UNKNOW_ERROR.getErrorCode());
+            responseData.put("errMsg", EmBusinessError.UNKNOW_ERROR.getErrMsg());
+        }
+
+        return CommonReturnType.create(responseData, "fail");
+
+    }
+}
--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -1,17 +1,22 @@
 package com.miaoshaproject.controller;
 
 import com.miaoshaproject.controller.viewobject.UserVO;
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBusinessError;
 import com.miaoshaproject.response.CommonReturnType;
 import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.UserModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.beans.BeanUtils.copyProperties;
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * @program: miaosha
@@ -21,16 +26,23 @@ import static org.springframework.beans.BeanUtils.copyProperties;
  **/
 @Controller("user")
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
 
     @RequestMapping("/get")
     @ResponseBody
-    public CommonReturnType getUser(@RequestParam(name = "id") Integer id) {
+    public CommonReturnType getUser(@RequestParam(name = "id") Integer id) throws BusinessException {
         //调用service服务获取对应的id用户对象并返回给前端
         UserModel userModel = userService.getUserById(id);
+
+        //若获取的对应用户信息不存在
+        if (userModel == null) {
+
+           throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+        }
+
 
         //将核心领域模型用户对象转化为可供UI使用的viewObject
         UserVO userVO = convertFromModel(userModel);
@@ -49,4 +61,7 @@ public class UserController {
         }
 
     }
+
+
+
 }
